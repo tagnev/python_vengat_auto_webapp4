945a31bf439c4ca095e3b918d72f1270cf138d86
LetsLearn <tagnev.vengat@gmail.com>
2024-05-16 06:54:52 -0700

    initial commit

---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
